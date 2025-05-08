b0f58ce73457bd90ff683921368b59125307c3f0
01010100heo <wikstromtheo@gmail.com>
2025-05-08 11:11:45 +0200

    Uppdatering

--- src/SDG_Main.java
+++ src/SDG_Main.java
@@ -3,17 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+
 /**
  *
  * @author theow
  */
 public class SDG_Main {
+    private static InfDB idb;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+                 try {
+            idb = new InfDB("sdgsweden", "3306", "dbAdmin2024", "dbAdmin2024PW");
+            
+        } catch (InfException ex){
+            System.out.println(ex.getMessage());
+        }
+        
     }
     
 }
